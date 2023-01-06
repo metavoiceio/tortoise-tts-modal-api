6e582d1081d1e581d7f31bdea819b0be0973ac2e
Vatsal <vatsalaggarwal@gmail.com>
2023-01-06 17:25:15 +0530

    feat: impelemnt modal app
    also,
    - update README
    - add linters to requirements

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# tortoise-tts-modal-api
+# 🐢 Tortoise TTS pay-as-you-go API
+
+A "pay-as-you-go" API for [Tortoise TTS](https://github.com/neonbjb/tortoise-tts/). It uses [Modal](https://modal.com/) underneath.
+
+## Developer environment
+- `python=3.10.8` (important for Modal)
---
+++ modal_app.py
@@ -0,0 +1,92 @@
+import asyncio
+import io
+import tempfile
+
+import modal
+from fastapi import Request
+from fastapi.responses import Response
+
+stub = modal.Stub("tts")
+
+tortoise_image = (
+    modal.Image.conda()
+    .apt_install("git")
+    .apt_install("libsndfile-dev")
+    .apt_install("ffmpeg")
+    .run_commands(
+        "pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu116",
+        "git clone https://github.com/vatsalaggarwal/tortoise-tts",
+        "cd tortoise-tts; pip install -r requirements.txt; pip install -e .",
+        # download models as part of the container.
+        "python -c 'from tortoise.api import MODELS_DIR, TextToSpeech;tts = TextToSpeech(models_dir=MODELS_DIR); tts.get_random_conditioning_latents()'",
+        "pip install pydub",
+    )
+)
+
+
+class Modal:
+    def __enter__(self):
+        from tortoise.api import MODELS_DIR, TextToSpeech
+        from tortoise.utils.audio import load_voices
+
+        self.load_voices = load_voices
+        self.tts = TextToSpeech(models_dir=MODELS_DIR)
+        self.tts.get_random_conditioning_latents()
+
+    def process_synthesis_result(self, result):
+        import pydub
+        import torchaudio
+
+        with tempfile.NamedTemporaryFile() as converted_wav_tmp:
+            torchaudio.save(
+                converted_wav_tmp.name + ".wav",
+                result,
+                24000,
+            )
+            wav = io.BytesIO()
+            _ = pydub.AudioSegment.from_file(
+                converted_wav_tmp.name + ".wav", format="wav"
+            ).export(wav, format="wav")
+
+        return wav
+
+    @stub.webhook(method="POST", image=tortoise_image, gpu="A100")
+    def run_tts(self, req: Request):
+        body = asyncio.run(req.json())
+        text = body["text"]
+        voices = body["voices"]
+
+        CANDIDATES = 1  # NOTE: this code only works for one candidate.
+        CVVP_AMOUNT = 0.0
+        SEED = None
+        PRESET = "fast"
+
+        # TODO: make work for multiple voices
+        selected_voices = voices.split(",")
+
+        selected_voice = selected_voices[0]
+
+        if "&" in selected_voice:
+            voice_sel = selected_voice.split("&")
+        else:
+            voice_sel = [selected_voice]
+        voice_samples, conditioning_latents = self.load_voices(voice_sel)
+
+        gen, _ = self.tts.tts_with_preset(
+            text,
+            k=CANDIDATES,
+            voice_samples=voice_samples,
+            conditioning_latents=conditioning_latents,
+            preset=PRESET,
+            use_deterministic_seed=SEED,
+            return_deterministic_state=True,
+            cvvp_amount=CVVP_AMOUNT,
+        )
+
+        wav = self.process_synthesis_result(gen.squeeze(0).cpu())
+
+        return Response(content=wav.getvalue(), media_type="audio/wav")
+
+
+if __name__ == "__main__":
+    stub.serve()
